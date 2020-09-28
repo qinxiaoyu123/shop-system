91f79e9d69de15d2fe88c57aedd4f73f16be7507
QinXiaoyu <1041411393@qq.com>
2020-09-28 17:20:46 +0800

    add shop controller

---
+++ src/main/java/com/tju/shop/util/HttpServletRequestUtil.java
@@ -0,0 +1,53 @@
+package com.tju.shop.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpServletRequestUtil {
+    public static int getInt(HttpServletRequest request, String key) {
+        try {
+            return Integer.decode(request.getParameter(key));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static long getLong(HttpServletRequest request, String key) {
+        try {
+            return Long.parseLong(request.getParameter(key));
+        } catch (Exception e) {
+            return -1L;
+        }
+    }
+
+    public static double getDouble(HttpServletRequest request, String key) {
+        try {
+            return Double.parseDouble(request.getParameter(key));
+        } catch (Exception e) {
+            return -1d;
+        }
+    }
+
+    public static boolean getBoolean(HttpServletRequest request, String key) {
+        try {
+            return Boolean.parseBoolean(request.getParameter(key));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getString(HttpServletRequest request, String key) {
+        try {
+            String result = request.getParameter(key);
+            if (request != null) {
+                result = result.trim();
+            }
+            if ("".equals(result)) {
+                return null;
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/tju/shop/web/shopadmin/ShopManagementController.java
@@ -0,0 +1,110 @@
+package com.tju.shop.web.shopadmin;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tju.shop.dto.ShopExecution;
+import com.tju.shop.entity.PersonInfo;
+import com.tju.shop.entity.Shop;
+import com.tju.shop.enums.ShopStateEnum;
+import com.tju.shop.service.ShopService;
+import com.tju.shop.util.HttpServletRequestUtil;
+import com.tju.shop.util.ImageUtil;
+import com.tju.shop.util.PathUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/shopadmin")
+public class ShopManagementController {
+    @Autowired
+    private ShopService shopService;
+    @RequestMapping(value = "registerShop",method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> registerShop(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        //1.接受并转换相应的参数，包括店铺信息以及图片信息
+        String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
+        ObjectMapper mapper = new ObjectMapper();
+        Shop shop = null;
+        try{
+            shop = mapper.readValue(shopStr,Shop.class);
+        }catch (Exception e){
+            modelMap.put("success", false);
+            modelMap.put("errMsg",e.getMessage());
+            return modelMap;
+        }
+        CommonsMultipartFile shopImg = null;
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if(commonsMultipartResolver.isMultipart(request)){
+            MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
+            shopImg = (CommonsMultipartFile)multipartHttpServletRequest.getFile("shopImg");
+        }else{
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "上传图片不能为空");
+            return modelMap;
+        }
+        //2.注册店铺
+        if(shop != null && shopImg != null){
+            PersonInfo owner = new PersonInfo();
+            owner.setUserId(1L);
+            shop.setOwner(owner);
+            File shopImgFile = new File(PathUtil.getImgBasePath()+ ImageUtil.getRandomFileName());
+            try{
+                shopImgFile.createNewFile();
+                inputStreamToFile(shopImg.getInputStream(),shopImgFile);
+            }catch (Exception e){
+                modelMap.put("success", false);
+                modelMap.put("errMsg",e.getMessage());
+                return modelMap;
+            }
+
+            ShopExecution se = shopService.addShop(shop, shopImgFile);
+            if(se.getState() == ShopStateEnum.CHECK.getState()){
+                modelMap.put("success", true);
+            }else{
+                modelMap.put("success", false);
+                modelMap.put("errMsg",se.getStateInfo());
+            }
+            return modelMap;
+        }else{
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请输入店铺信息");
+            return modelMap;
+        }
+        //3.返回结果
+    }
+    private static void inputStreamToFile(InputStream ins, File file){
+        OutputStream os = null;
+        try{
+            os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while((bytesRead = ins.read(buffer)) != -1){
+                os.write(buffer, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("调用inputStreamToFile产生异常："+e.getMessage());
+        } finally {
+            try{
+                if(os!= null){
+                    os.close();
+                }
+                if(ins!=null){
+                    ins.close();
+                }
+            }catch (IOException e){
+                throw new RuntimeException("inputStreamToFile关闭io产生异常："+e.getMessage());
+            }
+        }
+    }
+}
